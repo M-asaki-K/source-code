2a6f68d89845b4e408f976399d77d8700a3144b4
M-asaki-K <47930865+M-asaki-K@users.noreply.github.com>
2019-06-29 10:16:37 +0900

    Update Takedakaggle.R

--- Takedakaggle.R
+++ Takedakaggle.R
@@ -34,30 +34,24 @@ perm = sample(n, size = round(n * train_size))
 #-----------select x from the dataset-----------------
 x.0 <- complete.compounds[,-c(1)]
 x.0.t <- complete.compounds.t[,]
-
 x.sds <- apply(x.0, 2, sd)
 x.sds.t <- apply(x.0.t, 2, sd)
 
 sd.is.not.0 <- x.sds != 0 
 sd.is.not.0
 sd.is.not.0.t <- x.sds.t != 0
-sd.is.not.0.t
-x.0 <- x.0[, (sd.is.not.0) * (sd.is.not.0.t)]
-x.0.t <- x.0.t[, (sd.is.not.0) * (sd.is.not.0.t)]
+sd.is.not.0 <- sd.is.not.0.t&sd.is.not.0
+x.0 <- x.0[, (sd.is.not.0)]
+x.0.t <- x.0.t[, (sd.is.not.0)]
+x.0[200,]
 
-#pc = 100
-#rpca = prcomp(x = x.0,scale=T)
-#x.0 <- as.data.frame(rpca$x[,c(1:pc)])
+pc = 1000
+rpca = prcomp(x = x.0,scale=T)
+x.0 <- as.data.frame(rpca$x[,c(1:pc)])
 
-#rpca.t = prcomp(x = x.0.t,scale=T)
-#x.0.t <- as.data.frame(rpca.t$x[,c(1:pc)])
+rpca.t = prcomp(x = x.0.t,scale=T)
+x.0.t <- as.data.frame(rpca.t$x[,c(1:pc)])
 
-#sampledata <- as.data.frame(cbind(train_labels, train_data))
-#lsample <- lm(train_labels~., data = sampledata)
-#summary(lsample)
-#p <- predict(lsample)
-#plot(p, train_labels)
-#train_labels
 train_data <- cbind.data.frame(x.0[perm,])
 test_data <- cbind.data.frame(x.0[-perm,])
 #-----------select y from the dataset------------------
@@ -90,9 +84,9 @@ build_model <- function() {
                 input_shape = dim(train_data)[2]) %>%
     layer_batch_normalization() %>%
     layer_dropout(rate = 0.25) %>%
-#    layer_dense(units = 100, activation = "relu") %>%
-#    layer_batch_normalization() %>%
-#    layer_dropout(rate = 0.25) %>%
+    layer_dense(units = 100, activation = "relu") %>%
+    layer_batch_normalization() %>%
+    layer_dropout(rate = 0.25) %>%
     layer_dense(units = 1)
   
   model %>% compile(
@@ -104,9 +98,9 @@ build_model <- function() {
   model
 }
 
-epochs <- 300
+epochs <- 1000
 batch_size <- 128
-initial_lr<-0.01
+initial_lr<-0.1
 decay<-2
 period<-25
 
@@ -128,7 +122,6 @@ lr_schedule<-function(epoch,lr) {
   }
 cb_lr<-callback_learning_rate_scheduler(lr_schedule)
 early_stop <- callback_early_stopping(monitor = "val_loss", patience = 100)
-#rr <- callback_learning_rate_scheduler(function(epochs){0.01 / epochs})
 
 # Fit the model and store training stats
 history <- model %>% fit(
@@ -138,7 +131,7 @@ history <- model %>% fit(
   validation_split = 0.2,
   verbose = 0,
   batch_size = batch_size,
-  callbacks = list(print_dot_callback, cb_lr)
+  callbacks = list(print_dot_callback, cb_lr, early_stop)
 )
 
 plot(history)
@@ -147,29 +140,5 @@ train_predictions <- model %>% predict(train_data)
 plot(train_predictions, train_labels)
 r2train <- cor(train_predictions, train_labels)**2
 r2train
-
-#-----------read csv file as compounds--------------
-compounds.t <- read.csv('../input/testtakeda/test.csv')
-
-#-----------remove some columns if needed--------------
-trimed.compounds.t <- compounds.t[,-c(1)]
-
-#-----------select rows without empty cells---------
-is.completes.t <- complete.cases(trimed.compounds.t)
-is.completes.t
-
-complete.compounds.t <- trimed.compounds.t[is.completes.t,]
-
-#-----------select x from the dataset-----------------
-x.0.t <- complete.compounds.t[,]
-x.0.t <- x.0.t[, sd.is.not.0]
-
-rpca = prcomp(x = x.0.t,scale=T)
-x.0.t <- as.data.frame(rpca$x[,c(1:pc)])
-
-test_data.t <- cbind.data.frame(x.0.t[,])
-# Test data is *not* used when calculating the mean and std.
-# Use means and standard deviations from training set to normalize test set
-test_data.t <- scale(test_data.t, center = col_means_train, scale = col_stddevs_train)
 
 test_predictions.t <- model %>% predict(test_data.t)
